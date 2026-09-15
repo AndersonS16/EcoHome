715e4cdd05d745196517e0fa419192c9b60d7a1c
Anderson Santana <javis1316@gmail.com>
2026-09-14 22:07:01 -0500

    Preparando backend EcoHomeStore para Render

--- src/config/db.js
+++ src/config/db.js
@@ -1,16 +1,21 @@
 import pkg from 'pg';
+import 'dotenv/config';
+
 const { Pool } = pkg;
 
 const pool = new Pool({
-  host: 'localhost',
-  user: 'postgres',
-  password: 'postgres',
-  database: 'ecohome_db',
-  port: 5432,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 pool.on('connect', () => {
-  console.log('Conectado a PostgreSQL exitosamente');
+  console.log('Conectado a PostgreSQL en Supabase exitosamente');
 });
 
 export default pool;
